4cd70538a9d86b8fd143abf1e08ca5cd83f64bd8
Gecolay <Discord.Benjamin@web.de>
2024-12-18 19:27:56 +0100

    Test build gradle updates

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,17 +10,9 @@ dependencies {
     compileOnly("com.intellectualsites.plotsquared:plotsquared-core:7.4.0")
 }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
-}
-
 tasks {
     compileJava {
         options.release.set(16)
-    }
-
-    publishToMavenLocal {
-        dependsOn(build)
     }
 
     publishing {
